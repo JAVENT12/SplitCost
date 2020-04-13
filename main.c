2199bab798a357f4d844c82364351a9a7e6e465a
JAVENT12 <52118658+JAVENT12@users.noreply.github.com>
2020-04-13 17:31:53 -0400

    Update main.c

--- main.c
+++ main.c
@@ -94,5 +94,5 @@ int main(void)
 => C Functions: printf(),  scanf(), srand(), rand(), etc. -- Done
 => C Control Statements (if, if/else, switch) -- Done
 => C Loop Statements (for, while, do) -- Done
-=> Any other C skill you'd like to demonstrate -- Handle incorrect input/exceptions, Not completetly done
+=> Any other C skill you'd like to demonstrate -- Handle incorrect input/exceptions
 */
